76b3a35441714b1028806ac1814b5fb7a033ce55
Ghadah-Ahmed <moslem.mah@gmail.com>
2021-11-24 16:20:05 +0300

    m

--- front-end/front/src/App.js
+++ front-end/front/src/App.js
@@ -7,6 +7,27 @@ function App() {
   const [authors, setAuthors] = React.useState([])
   // const [display, setDisplay] = React.useState(false)
   const [nameD, setNameD] = React.useState('')
+  const [author, setAuthor] = React.useState({
+      name: "",
+      age: 0,
+      nationality: "",
+      image: "",
+      gender: "",
+      books: []
+  })
+
+  const [authorU, setAuthorU] = React.useState({
+    name: "",
+    age: 0,
+    nationality: "",
+    image: "",
+    gender: "",
+    books: []
+})
+
+  React.useEffect(()=>{
+    console.log(author)
+  }, [author])  
 
   React.useEffect(()=>{
     axios.get('http://localhost:3001/authors')
@@ -33,14 +54,7 @@ function App() {
 
   const postAuthor = (e) =>{
     e.preventDefault()
-    axios.post('http://localhost:3001/authors',  {
-    name: "Whatever",
-      nationality:"Saudi",
-      image: "null",
-      gender: "male" ,
-      books: [],
-      age: 33
-  })
+    axios.post('http://localhost:3001/authors',  author)
     .then((res) => {          
       console.log(res)
     })
@@ -52,14 +66,7 @@ function App() {
 
   const updateAuthor = (e) =>{
     e.preventDefault()
-    axios.put('http://localhost:3001/authors',  {
-    name: "Whatever",
-      nationality:"Saudi",
-      image: "null",
-      gender: "female" ,
-      books: [],
-      age: 44
-  })
+    axios.put('http://localhost:3001/authors',  authorU)
     .then((res) => {          
       console.log(res)
     })
@@ -87,16 +94,27 @@ function App() {
       <input onChange={(e)=> setNameD(e.target.value)} placeholder="author name"/>
       <button onClick={(e)=>deleteAuthor(e)} >Delete Author</button>
     </form>
-
+<br/>
     <form>
-      <input onChange={(e)=> setNameD(e.target.value)} placeholder="author name"/>
+      <input onChange={(e)=> setAuthor({...author, ['name']: e.target.value})} placeholder="author name"/>
+      <input onChange={(e)=> setAuthor({...author, ['age']: parseInt(e.target.value)})} placeholder="author age"/>
+      <input onChange={(e)=> setAuthor({...author, ['nationality']: e.target.value})} placeholder="author nationality"/>
+      <input onChange={(e)=> setAuthor({...author, ['gender']: e.target.value})} placeholder="author gender"/>
+      <input onChange={(e)=> setAuthor({...author, ['image']: e.target.value})} placeholder="author image"/>
+      <input onChange={(e)=> setAuthor({...author, ['books']: e.target.value})} placeholder="author books"/>
       <button onClick={(e)=>postAuthor(e)} >Post Author</button>
     </form>
-
+<br/>
     <form>
-      <input onChange={(e)=> setNameD(e.target.value)} placeholder="author name"/>
+      <input onChange={(e)=> setAuthorU({...authorU, ['name']: e.target.value})} placeholder="author name"/>: <br/>
+      <input onChange={(e)=> setAuthorU({...authorU, ['age']: parseInt(e.target.value)})} placeholder="author age"/>
+      <input onChange={(e)=> setAuthorU({...authorU, ['nationality']: e.target.value})} placeholder="author nationality"/>
+      <input onChange={(e)=> setAuthorU({...authorU, ['gender']: e.target.value})} placeholder="author gender"/>
+      <input onChange={(e)=> setAuthorU({...authorU, ['image']: e.target.value})} placeholder="author image"/>
+      <input onChange={(e)=> setAuthorU({...authorU, ['books']: e.target.value})} placeholder="author books"/>
       <button onClick={(e)=>updateAuthor(e)} >Update Author</button>
     </form>
+<br/>
 
     </div>
   );

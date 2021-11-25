24920a7ae2a38a3bf3909b4e473d79936edb61e7
Maha Alharbi <m.a.alharbi995@gmail.com>
2021-11-25 10:25:45 +0300

    add passport in user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = new require('mongoose'),
 {Schema} = mongoose
 
+// passportLocalMongoose is a library in npm (تسهل عملية signup , sign in)  
+const passportLocalMongoose = require('passport-local-mongoose')
 const UserSchema = new Schema ({
     name:{
         type:String,
@@ -16,5 +18,10 @@ const UserSchema = new Schema ({
         required: [true, "Email should be provided"]
     }
 })
-
+//passportLocalMongoose  تسمح بتعريف معلومات اليوزر 
+//plugin to user model 
+//2nd arrgument => authentication اعطيه الايميل كيوزر نيم فيلد لعملية 
+UserSchema.plugin(passportLocalMongoose,{
+    usernameField = 'email'
+})
 module.exports = mongoose.model('User',UserSchema);

f3e1c2fd397e2a6f8a3c9c3812e6c2a9cf7f38aa
Emad Aldomari <ea@ea.sa>
2021-11-30 10:09:39 +0300

    update schema like the what hanin provided

--- backend/models/author.js
+++ backend/models/author.js
@@ -1,8 +1,14 @@
 const { Schema, model } = require("mongoose");
 const Book = require('./book');
-
+const {isEmail} = require("validator");
 module.exports = model('Author', new Schema({ 
-    email: String,
+    email: {
+        type: String,
+        required: [true, "email should be provided"],
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, "email is invaild"]
+    },
     password: String,
     name: {
         type: String,

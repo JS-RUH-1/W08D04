4468e430abcb960f4dafdbf322d4f779e5f97586
Maha Alharbi <m.a.alharbi995@gmail.com>
2021-12-01 03:07:08 +0300

    navbar => login, regester

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,31 +1,22 @@
 
 import { Link } from "react-router-dom";
-import {useContext,useState } from "react"
+import {useContext } from "react"
 import { ContextStore } from "../context";
-// // import { showLogin, showSignup } from "../reducers/assets";
-// // import UserDropdown from "./UserDropdown";
 
 export default function Navbar() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const { author, setToken } = useContext(ContextStore);
-
-
-//   // const state = useSelector((state) => ({ ...state.assets }));
-//   // const dispatch = useDispatch();
-//   // const [isMenuOpen, setIsMenuOpen] = useState(false);
+const { author , setAuthor} = useContext(ContextStore);
 
   return (
-    <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-    <div class="relative flex items-center justify-between">
-      <div class="flex items-center">
+    <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+    <div className="relative flex items-center justify-between">
+     
+      <div className="flex items-center">
         <Link
           to=""
-          aria-label="Company"
-          title="Company"
-          class="inline-flex items-center mr-8"
+          className="inline-flex items-center mr-8"
         >
           <svg
-            class="w-8  text-pink-700"
+            className="w-8  text-pink-700"
             viewBox="0 0 24 24"
             strokeLinejoin="round"
             strokeWidth="2"
@@ -39,17 +30,15 @@ export default function Navbar() {
             <rect x="14" y="1" width="7" height="6" />
             <rect x="14" y="11" width="7" height="12" />
           </svg>
-          <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+          <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
             Library
           </span>
         </Link>
-        <ul class="flex items-center hidden space-x-8 lg:flex">
+        <ul className="flex items-center hidden space-x-8 lg:flex">
           <li>
             <Link
               to="/books"
-              aria-label="Our product"
-              title="Our product"
-              class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-pink-700"
+              className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-pink-700"
             >
               Books
             </Link>
@@ -57,40 +46,53 @@ export default function Navbar() {
           <li>
             <Link
               to="/authors"
-              aria-label="Our product"
-              title="Our product"
-              class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-pink-700"
+              className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-pink-700"
             >
               Authors
             </Link>
           </li>
-        
-         
         </ul>
       </div>
-      <ul class="flex items-center hidden space-x-8 lg:flex">
+    
+    
+      <ul className="flex items-center  space-x-8 lg:flex">
+      {author ? <>
         <li>
-          <a
-            href="/login"
-            aria-label="Sign in"
-            title="Sign in"
-            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-pink-700"
+          <Link
+            to=""
+            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-pink-700"
+          > Hello {author.name} </Link> </li>    
+        <li>
+          <Link
+            to=""
+            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-pink-700"
+            onClick={() =>{
+              setAuthor(undefined);
+              localStorage.removeItem("auth");
+            }}
+          > Logout </Link> </li>
+        </> : <>
+        <li>
+          <Link
+          to="/login"
+            className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-pink-700 hover:bg-pink-200 focus:shadow-outline focus:outline-none"
           >
             Login
-          </a>
+          </Link>
         </li>
+
         <li>
-          <a
-            href="/signup"
-            class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-pink-700 hover:bg-pink-200 focus:shadow-outline focus:outline-none"
-            aria-label="Sign up"
-            title="Sign up"
+          <Link
+          to="/signup"
+            className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-pink-700 hover:bg-pink-200 focus:shadow-outline focus:outline-none"
           >
             Regester
-          </a>
+          </Link>
         </li>
+        </>}
       </ul>
     </div>
   </div>
+
   );
 }
